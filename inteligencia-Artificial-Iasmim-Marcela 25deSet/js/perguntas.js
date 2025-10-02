76adfdd04f3f1a753bf9e86441584ed5544791ae
IasmimLerner <142436656+IasmimLerner@users.noreply.github.com>
2025-10-02 13:58:00 -0300

    Add files via upload

---
+++ inteligencia-Artificial-Iasmim-Marcela 25deSet/js/perguntas.js
@@ -0,0 +1,107 @@
+export const perguntas = [
+    {
+        enunciado: "Você descobre que estão criando Inteligência Artificial, também conhecida como IA para facilitar o dia a dia das pessoas. O que você pensaria sobre essa nova ideia?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [
+                    "No início ficou com medo do que essa tecnologia pode fazer.",
+                    "Achou que a IA poderia ultrapassar o controle humano e trazer consequências imprevisíveis."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: [
+                    "Quis saber como usar IA no seu dia a dia.",
+                    "Viu um grande potencial para melhorar a vida das pessoas, principalmente nas tarefas diárias."
+                ],
+                proxima: 2,
+            }
+        ]
+    },
+    {
+        enunciado: "Depois de um tempo os professores, decobrem essa Inteligência Artificial estudam sobre. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Ficaria assustado pois seria algo novo para todos.",
+                afirmacao: [
+                    "Conseguiu utilizar a IA para buscar informações úteis.",
+                    "Se sentiu um pouco perdido, mas viu que a IA poderia facilitar a pesquisa e organização de ideias."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: [
+                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
+                    "Percebeu que a IA não substitui o pensamento crítico e que as ideias próprias fazem toda a diferença."
+                ],
+                proxima: 4,
+            }
+        ]
+    },
+    {
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode ajudar muito para criar novas coisas e melhorar, e criar mais oportunidade de emprego.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Acredita que a IA pode liberar as pessoas para atividades mais criativas e estratégicas."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Acredita que a IA deve ser usada de forma responsável para não causar impactos negativos no mercado de trabalho."
+                ],
+                proxima: 4,
+            }
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem pesquisando na internet algo sobre para tirar ideias.",
+                afirmacao: [
+                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
+                    "Percebeu que muitas imagens disponíveis na internet não expressam o que realmente queria transmitir e decidiu fazer a imagem do zero."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: [
+                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
+                    "Viu que usar IA para gerar imagens pode ser uma ferramenta poderosa, mas entende que é importante manter o toque humano na arte."
+                ],
+                proxima: 6,
+            }
+        ]
+    },
+    {
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        alternativas: [
+            {
+                texto: "Escrever comandos para o chat nós ajudar com o trabalho que está atrasado, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: [
+                    "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo.",
+                    "Percebeu que ao depender da IA para completar as tarefas, está deixando de aprender o conteúdo por conta própria."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: [
+                    "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo.",
+                    "Compreendeu que usar a IA como ferramenta de auxílio é válido, mas que o aprendizado pessoal deve sempre vir em primeiro lugar."
+                ],
+                proxima: 7,
+            }
+        ]
+    },
+];
